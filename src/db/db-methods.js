f86b765daa8933759ecd60d4cec3cb9a8a2614d0
OrynaChubych <oryna.chubych@gmail.com>
2020-01-30 03:36:41 +0200

    Refactor structure and db methods

--- src/db/db-methods.js
+++ src/db/db-methods.js
@@ -1,16 +1,58 @@
 const Lesson = require("../models/models");
 
-function deleteLesson(id) {
-    Lesson.findOneAndDelete({
+const saveLesson = reqBody => {
+    const newLesson = new Lesson(reqBody);
+    return newLesson.save();
+}
+
+const deleteLesson = id => {
+    return Lesson.findOneAndDelete({
         _id: id
     });
 };
 
-function saveLesson(objectToSave) {
-    return objectToSave.save()
-};
+const readLesson = id => {
+    return Lesson.findOne({
+        _id: id
+    });
+}
+const editLesson = (id, reqBody) => {
+    return Lesson.findOneAndUpdate({
+        _id: id
+    }, {
+        $set: reqBody
+    }, {
+        new: true
+    });
+}
+const listLessons = () => {
+    return Lesson.find({});
+}
+//editing a room number because of renovating a school room as a example of bulk edition
+const editManyLessons = reqBody => {
+    return Lesson.updateMany({
+        room_number: reqBody.room_number_old
+    }, {
+        $set: {
+            room_number: reqBody.room_number_new
+        }
+    });
+}
+//in order not to delete all documents, as a example
+// we can delete documents which satisfy a certain query,
+// e.g. no lessons will take place in certain room
+const deleteManyLessons = reqBody => {
+    return Lesson.deleteMany({
+        room_number: reqBody.room_number
+    });
+}
 
-// to do readLesson (), editLesson (), editManyLessons (), deleteManyLessons, listLessons ()
-
-module.exports = saveLesson;
-module.exports = deleteLesson;;
+module.exports = {
+    saveLesson,
+    deleteLesson,
+    readLesson,
+    deleteManyLessons,
+    listLessons,
+    editManyLessons,
+    editLesson
+}
